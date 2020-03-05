2fe425fb6caa82482ff759167c7aa4ce221a241d
GerryNdungu <gerryjerry36@gmail.com>
2020-03-05 17:19:43 +0300

    Created admin view, controller and admin is able to view list of projects and bugs

---
+++ resources/views/admin/bugs.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.master')
+
+@section('title')
+    Bugs | BugTraq
+@endsection
+
+
+@section('content')
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-9">
+                                <h4 class="card-title"> List of Bugs</h4></div>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class=" text-primary">
+                                    <th>
+                                        Id
+                                    </th>
+                                    <th>
+                                        Priority
+                                    </th>
+                                    <th>
+                                        Bug Title
+                                    </th>
+
+                                    <th>
+                                        Created
+                                    </th>
+                                    <th>
+                                        Reporter
+                                    </th>
+                                    <th>
+                                        Dev Assigned
+                                    </th>
+                                    <th>
+                                        Due Date
+                                    </th>
+
+                                    <th class="text-right">
+                                        Status
+                                    </th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($bugs as $bug)
+                                        <tr>
+                                            <td>{{$bug->id}}></td>
+                                            <td>{{$bug->priority}}</td>
+                                            <td>{{$bug->title}}</td>
+                                            <td>{{$bug->created_at}}</td>
+                                            <td>{{$bug->reporter}}</td>
+                                            <td>{{$bug->assigned}}</td>
+                                            <td>{{$bug->due_date}}</td>
+                                            <td>{{$bug->status}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,8 @@ Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
-
+    Route::get('/projects','AdminController@showprojects')->name('admin.project');
+    Route::get('/bugs','AdminController@showbugs')->name('admin.bug');
 });
 
 Route::middleware(['auth'])->group(function () {
