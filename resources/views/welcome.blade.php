06a8f1feefb50e6cbaad87789518e1996bb84e4b
GerryNdungu <gerryjerry36@gmail.com>
2020-03-10 15:27:32 +0300

    Merge branch 'Dash' of C:\wamp64\www\BugTraq with conflicts.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,11 +69,13 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
+
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
+                        <li><a href="{{ route('admin.login') }}"> Admin Login</a></li>
                 </div>
             @endif
 
